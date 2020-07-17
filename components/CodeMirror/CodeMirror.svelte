99e4550969ef454b078357f5d8d624677abbb5fd
spencer kelly <spencermountain@gmail.com>
2020-07-17 12:00:58 -0400

    codemirror component cleanup

---
+++ components/CodeMirror/CodeMirror.svelte
@@ -0,0 +1,52 @@
+<script>
+  import { onMount } from 'svelte'
+  import CodeMirror from './lib.js'
+  export let text = ''
+  export let autofocus = true
+  let editor
+  let el
+  export let highlight = () => {}
+  export let onEnter = e => {
+    return CodeMirror.Pass
+  }
+
+  const clear = function(doc) {
+    doc.getAllMarks().forEach(m => m.clear())
+  }
+
+  onMount(() => {
+    editor = CodeMirror.fromTextArea(el, {
+      autofocus: autofocus,
+      extraKeys: {
+        Enter: onEnter,
+      },
+    })
+
+    editor.on('change', doc => {
+      clear(doc)
+      text = doc.getValue()
+      let offsets = highlight(text)
+      offsets.forEach(m => {
+        let start = doc.posFromIndex(m.start)
+        let end = doc.posFromIndex(m.end)
+        editor.markText(start, end, {
+          className: m.tag,
+        })
+      })
+    })
+    CodeMirror.signal(editor, 'change', editor)
+  })
+</script>
+
+<style>
+  :global(.CodeMirror) {
+    height: 3rem !important;
+    width: 35rem !important;
+  }
+</style>
+
+<svelte:head>
+  <link rel="stylesheet" href="./components/CodeMirror/style.css" />
+</svelte:head>
+
+<textarea class="textarea" bind:this={el}>{text}</textarea>
--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script>
   import User from './user/User.svelte'
   import Login from './Login.svelte'
-  import CodeMirror from './CodeMirror/CodeMirror.svelte'
+  import CodeMirror from '../components/CodeMirror/CodeMirror.svelte'
   import { user, pass, data } from './store'
   const logout = function() {
     $user = ''
@@ -10,9 +10,22 @@
     localStorage.setItem('pass', '')
     $data = {}
   }
+  const submit = () => {}
+  const highlight = function(str = '') {
+    let matches = [...str.matchAll(/\.[^\s\.]+/g)]
+    return matches.map(m => {
+      return {
+        start: m.index,
+        end: m[0].length + m.index,
+        tag: 'tag',
+      }
+    })
+  }
+  let text = 'in the .town where i was born.. there liv.ed a man'
 </script>
 
-<CodeMirror />
+<CodeMirror bind:text {highlight} />
+{text}
 {#if !$user}
   <!-- <Login /> -->
 {:else}
